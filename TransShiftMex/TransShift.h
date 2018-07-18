252ef5cb2028c25a5613dfe7550567216e2f9eba
Carrie Niziolek <cniziolek@wisc.edu>
2018-07-18 13:58:07 -0500

    Add param fb2Gain to control gain in noise-only mode (2)

--- TransShiftMex/TransShift.h
+++ TransShiftMex/TransShift.h
@@ -454,6 +454,8 @@ private:
 		mytype rmsFF;                   // rms fogetting factor for long time rms 
 		mytype rmsFF_fb[4];             // rms fogetting factor for fb mode 4 {ff_onset, ff_final, trans_time (s), ending time (s)} 
 
+		mytype fb2Gain;					// gain (scaling factor) of the noise under fb mode 2
+
 		mytype fb3Gain;					// gain (scaling factor) of the blended noise under fb mode 3
 
 		mytype fb4GainDB;				// gain (in dB) of the speech-modulated noise under fb mode 4
